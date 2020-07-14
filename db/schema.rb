2c250d5ce04e79f127e4a08bf78a12bd80fa192d
chinweokwu <paulcmorah@gmail.com>
2020-07-14 15:53:13 +0100

    worked with model in console

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema.define(version: 2020_07_14_022248) do
 
   create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
